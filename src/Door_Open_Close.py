66758c03704897e46138299f3141522ae25ce878
AydenBravender <aydenbravender@gmail.com>
2023-09-18 16:21:57 -0600

    added src file

---
+++ src/Door_Open_Close.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 17:18:05 2023
+
+@author: Bravender
+"""
+
+import random
+import math
+
+prime = 2**1279
+prime = prime - 1
+print(prime)
+print("HI")
+
+prime1 = 2**607
+prime1 = prime1 - 1
+print(prime1)
+
+
+semiprime = prime*prime1
+print("HI")
+print(semiprime)
+
+sum = prime + prime1
+sum1 = int(sum//2)
+print("hi")
+print(int(sum1))
+
+
+
+
+
---
+++ src/Hand_Pose_Detection.py
@@ -0,0 +1,15 @@
+# Up Arrow
+print("↑")
+
+# Down Arrow
+print("↓")
+
+import matplotlib.pyplot as plt
+
+# Up Arrow
+plt.plot([0, 1, 2], [0, 1, 0])
+
+# Down Arrow
+plt.plot([0, 1, 2], [1, 0, 1])
+
+plt.show()
