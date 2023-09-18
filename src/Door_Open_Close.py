e0b1e1aa4eec2bf07f8c59293da9706601d11184
AydenBravender <aydenbravender@gmail.com>
2023-09-18 16:24:26 -0600

    added src file

--- src/Door_Open_Close.py
+++ src/Door_Open_Close.py
@@ -1,33 +1,58 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 18 17:18:05 2023
+import socket
+import RPi.GPIO as GPIO
+import time
+steer_1 = 24 # pin 7
+steer_2 = 23 # pin 5
+enA = 25 # pin 3
 
-@author: Bravender
-"""
+GPIO.setmode(GPIO.BCM)
 
-import random
-import math
+pins = [enA,steer_1,steer_2]
 
-prime = 2**1279
-prime = prime - 1
-print(prime)
-print("HI")
+for element in pins:
+GPIO.setup(element,GPIO.OUT) # to set up all GPIO pins
+if element != enA:
+GPIO.output(element,GPIO.LOW) # to set all drive pins low
 
-prime1 = 2**607
-prime1 = prime1 - 1
-print(prime1)
-
-
-semiprime = prime*prime1
-print("HI")
-print(semiprime)
-
-sum = prime + prime1
-sum1 = int(sum//2)
-print("hi")
-print(int(sum1))
+steer=GPIO.PWM(enA,1000) # '1000' hertz duty cycle
+steer.start(100)
 
 
+mediapipeRe_socket = socket.socket()
+mediapipeRe_socket.bind(('127.0.0.1', 11312))
+mediapipeRe_socket.listen(1)
 
+try:
+    while True:
+        print("Waiting for a connection...")
+        client_socket, client_address = mediapipeRe_socket.accept()
+        print(f"Accepted connection from {client_address}")
 
+        while True:
+            received_data = client_socket.recv(1024).decode()
+            if not received_data:
+                break
+            print(received_data)
+           
+            try:
+                if received_data == "opened":
+                    GPIO.output(steer_1,GPIO.HIGH) # opens the door
+                    GPIO.output(steer_2,GPIO.LOW)
+                elif received_data == "clenched":
+                   GPIO.output(steer_1,GPIO.LOW) # closes the door
+                   GPIO.output(steer_2,GPIO.HIGH)
+                elif received_data == "neither": # leaves the door where it is
+                   GPIO.output(steer_1,GPIO.LOW)
+                   GPIO.output(steer_2,GPIO.LOW)
+                else:
+                    print("Invalid command")
+                 
+            except Exception as e:
+                print(f"Error: {e}")
 
+        print(f"Connection closed")
+        client_socket.close()
+
+finally:
+    GPIO.cleanup()
+    mediapipeRe_socket.close()
--- src/Hand_Pose_Detection.py
+++ src/Hand_Pose_Detection.py
@@ -1,15 +1,74 @@
-# Up Arrow
-print("↑")
+import cv2
+import math
+import mediapipe as mp
+import socket
+import time
 
-# Down Arrow
-print("↓")
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 
-import matplotlib.pyplot as plt
+# Initialize video capture
+cap = cv2.VideoCapture(0)
 
-# Up Arrow
-plt.plot([0, 1, 2], [0, 1, 0])
+# Initialize Mediapipe hands
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+    client_socket = socket.socket()
+    # Change '127.0.0.1' with your computers ip address
+    client_socket.connect(('127.0.0.1', 11312))
 
-# Down Arrow
-plt.plot([0, 1, 2], [1, 0, 1])
+    last_processing_time = time.time()
+    processing_interval = 0.3  # Process a frame every 0.3 seconds, this way the recieving program doesn't get overloaded with data
+    
+    while True:
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+        
+        current_time = time.time() # helps the program know when the last time it processed a frame was, and when it needs to
+        if current_time - last_processing_time >= processing_interval:
+            last_processing_time = current_time
 
-plt.show()
+            results = hands.process(image)
+
+            if results.multi_hand_landmarks is not None:
+                for hand_landmarks in results.multi_hand_landmarks:
+                   # we find the position on the screen of these 4 points: 
+                    index_metacarpal = (hand_landmarks.landmark[5].x, hand_landmarks.landmark[5].y, hand_landmarks.landmark[5].z)
+                    pinky_metacarpal = (hand_landmarks.landmark[20].x, hand_landmarks.landmark[20].y, hand_landmarks.landmark[20].z)
+                    middle_tip = (hand_landmarks.landmark[14].x, hand_landmarks.landmark[14].y, hand_landmarks.landmark[14].z)
+                    metacarpal = (hand_landmarks.landmark[10].x, hand_landmarks.landmark[10].y, hand_landmarks.landmark[10].z)
+
+                    # Calculate the 2D distance (ignoring depth)
+                    distance_base = math.sqrt((index_metacarpal[0] - pinky_metacarpal[0])**2 + (index_metacarpal[1] - pinky_metacarpal[1])**2)
+                    distance_var = math.sqrt((middle_tip[0] - metacarpal[0])**2 + (middle_tip[1] - metacarpal[1])**2)
+                    # we measure the width of the hand (distance_base), and distance of the base of the middle finger to the tip
+                    # when we divide these 2 numbers we get a value that depending on your hand size determine whether its opened or clenched
+                    value = distance_base/distance_var
+                    
+                    # The number 3 here is what works for my hands, but you'll need to play around with a value that works better for you
+                    # you can add 'print(value)' to see what it is when your hand is opened and closed
+                    if value > 3:
+                        hand = "opened"
+                    elif value < 3:
+                        hand = "clenched"
+                    # This is an optional line of code that draws all of the hand points onto the image
+                    mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    # Also optional, it writes the status of the variable 'hand' onto the screen
+                    cv2.putText(image, f'Hand: {hand}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+            else:
+                hand = "neither"
+
+            data_to_send = hand
+            client_socket.send(data_to_send.encode())
+           
+            cv2.imshow('MediaPipe Hands', image)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+client_socket.close()
